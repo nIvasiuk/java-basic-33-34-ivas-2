11c9510747ca406b2f488ed54dc1f4ce1f416090
nIvasiuk <nadin.ivasiuk@gmail.com>
2022-06-20 02:39:49 +0200

    Homework 002.2 "Cat"  to check

---
+++ src/main/java/com/berlin/telran/practicesecond/Cat.java
@@ -0,0 +1,71 @@
+package com.berlin.telran.practicesecond;
+
+public class Cat {
+    String breed;
+    int age;
+    String name;
+    String colour;
+    int height;
+    int weight;
+    String eyeColour;
+
+
+    public void setBreed(String text) {
+        breed = text;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setAge(int number) {
+        age = number;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String text) {
+        name = text;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setColour(String text) {
+        colour = text;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setHeight(int number) {
+        height = number;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setWeight(int number) {
+        weight = number;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+
+    public void setEyeColour(String text) {
+        eyeColour = text;
+    }
+
+    public String getEyeColour() {
+        return eyeColour;
+    }
+
+}
+
---
+++ src/main/java/com/berlin/telran/practicesecond/Main.java
@@ -0,0 +1,39 @@
+package com.berlin.telran.practicesecond;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Cat myCat = new Cat();
+        myCat.setBreed("Tiger");
+
+        System.out.println(myCat.getBreed());
+
+        myCat.setAge(15);
+
+        System.out.println(myCat.getAge());
+
+        myCat.setName("Barsik");
+
+        System.out.println(myCat.getName());
+
+        myCat.setColour("Black");
+
+        System.out.println(myCat.getColour());
+
+
+        myCat.setHeight(30);
+
+        System.out.println(myCat.getHeight());
+
+        myCat.setWeight(5);
+
+        System.out.println(myCat.getWeight());
+
+        myCat.setEyeColour("Green");
+
+        System.out.println(myCat.getEyeColour());
+
+            }
+
+
+}
