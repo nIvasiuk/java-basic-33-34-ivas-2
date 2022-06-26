6dc3588583e3f9d1263b1d8ad423c07c99de7b88
nIvasiuk <nadin.ivasiuk@gmail.com>
2022-06-26 18:08:34 +0200

    getter-setter

--- src/main/java/com/berlin/telran/homework/game/hero/Elf.java
+++ src/main/java/com/berlin/telran/homework/game/hero/Elf.java
@@ -8,4 +8,52 @@ public class Elf {
     private String helmet;
     private String armor;
     private String shoose;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(String weapon) {
+        this.weapon = weapon;
+    }
+
+    public String getHelmet() {
+        return helmet;
+    }
+
+    public void setHelmet(String helmet) {
+        this.helmet = helmet;
+    }
+
+    public String getArmor() {
+        return armor;
+    }
+
+    public void setArmor(String armor) {
+        this.armor = armor;
+    }
+
+    public String getShoose() {
+        return shoose;
+    }
+
+    public void setShoose(String shoose) {
+        this.shoose = shoose;
+    }
 }
