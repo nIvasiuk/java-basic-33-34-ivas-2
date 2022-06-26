d979592d36fdcb0d22f4390952aaa526c4e63f7e
nIvasiuk <nadin.ivasiuk@gmail.com>
2022-06-26 18:30:21 +0200

    done

--- src/main/java/com/berlin/telran/homework/game/hero/Constant.java
+++ src/main/java/com/berlin/telran/homework/game/hero/Constant.java
@@ -2,9 +2,11 @@ package com.berlin.telran.homework.game.hero;
 
 public class Constant {
 
-    static int ELF_POWER;
+    static  int ELF_POWER;
     static int ELF_CHARISMA;
     static int ELF_MAGIC;
+
+    // waht about - final ?
 
 
 
