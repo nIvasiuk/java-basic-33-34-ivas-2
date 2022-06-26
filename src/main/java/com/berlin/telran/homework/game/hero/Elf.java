2ae5ce962b946c545db16a5580e7b0111cf146b8
nIvasiuk <nadin.ivasiuk@gmail.com>
2022-06-26 18:02:51 +0200

    ed new pafametrs for Elf

--- src/main/java/com/berlin/telran/homework/game/hero/Elf.java
+++ src/main/java/com/berlin/telran/homework/game/hero/Elf.java
@@ -1,4 +1,11 @@
 package com.berlin.telran.homework.game.hero;
 
 public class Elf {
+
+    int id;
+    String name;
+    String weapon;
+    String helmet;
+    String armor;
+    String shoose;
 }
