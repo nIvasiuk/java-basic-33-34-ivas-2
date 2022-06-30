0b9cf0d6d52f1038ac4433a50d33e0f4b08d90f0
nIvasiuk <nadin.ivasiuk@gmail.com>
2022-06-30 11:32:52 +0200

    done numbers

--- src/main/java/com/berlin/telran/practice/number_system/NumberSystem.java
+++ src/main/java/com/berlin/telran/practice/number_system/NumberSystem.java
@@ -2,18 +2,29 @@ package com.berlin.telran.practice.number_system;
 
 public class NumberSystem {
     public static void main(String[] args) {
-        int a = 0b11;   // binary format 0b.....(0-2)
+        int a = 0b11110100;   // binary format 0b.....(0-2)
 
-        int b = 01234; // okto format prefix 0.....(0-7)
+        int b = 0364; // okto format prefix 0.....(0-7)
 
-        int c = 4569; // decimal format .... (0-9)
+        int c = 244; // decimal format .... (0-9)
 
-        int d = 0x141D12; // hex format prefix 0x ..... (0-9,A,B,C,D,E)
+        int d = 0xF4; // hex format prefix 0x ..... (0-9,A,B,C,D,E)
+
+     //   String s = "0x141D12";
+// char, doubl, long, float, short
+        // byte = -127    +127
+        // int = -214......    +214....    2
+
+//        String s1 = Integer.toHexString(c);
+//        System.out.println( "int to hex = " + Integer.toHexString(0b11));
+//        System.out.println( "int to hex = " + Integer.toHexString(b));
+
 
         System.out.println("Binary =" + a);
         System.out.println("Okto =" + b);
         System.out.println("Decimal =" + c);
         System.out.println("Hex =" + d);
+        //System.out.println("String Hex =" + s);
 
     }
 }
