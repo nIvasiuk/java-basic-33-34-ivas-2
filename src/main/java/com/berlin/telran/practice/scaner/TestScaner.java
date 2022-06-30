e86f82571bab39c200a9622c9c4aadefd9d94469
nIvasiuk <nadin.ivasiuk@gmail.com>
2022-06-30 11:45:48 +0200

    add coment//

--- src/main/java/com/berlin/telran/practice/scaner/TestScaner.java
+++ src/main/java/com/berlin/telran/practice/scaner/TestScaner.java
@@ -6,7 +6,7 @@ public class TestScaner {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in); //  System.in - insert from keypad
         System.out.println("Enter the number:");
-        int number = sc.nextInt(); // waiting for your taping...
+        int number = sc.nextInt(); // waiting for your taping... (only ones)
         System.out.println("Thank you! You entred a number " + number);
         sc.close();
     }
