e6861887b8f39ff8c0b22c0c3d8fd6aba88301b7
nIvasiuk <nadin.ivasiuk@gmail.com>
2022-06-30 11:50:13 +0200

    add second number

--- src/main/java/com/berlin/telran/practice/scaner/TestScaner.java
+++ src/main/java/com/berlin/telran/practice/scaner/TestScaner.java
@@ -6,8 +6,12 @@ public class TestScaner {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in); //  System.in - insert from keypad
         System.out.println("Enter the number:");
+
         int number = sc.nextInt(); // waiting for your taping... (only ones)
         System.out.println("Thank you! You entred a number " + number);
+        System.out.println("Enter the number:");
+        int number2 = sc.nextInt();
+        System.out.println("sum = " + (number + number2));
         sc.close();
     }
 
