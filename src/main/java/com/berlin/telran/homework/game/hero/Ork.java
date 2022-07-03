3bdca32b148136cf13a75e6468eedbc98b350dd5
nIvasiuk <nadin.ivasiuk@gmail.com>
2022-07-03 22:22:46 +0200

    add getter-setter

--- src/main/java/com/berlin/telran/homework/game/hero/Ork.java
+++ src/main/java/com/berlin/telran/homework/game/hero/Ork.java
@@ -9,4 +9,44 @@ public class Ork {
     private int fortune;
     private String armor;
 
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isGender() {
+        return gender;
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getFortune() {
+        return fortune;
+    }
+
+    public void setFortune(int fortune) {
+        this.fortune = fortune;
+    }
+
+    public String getArmor() {
+        return armor;
+    }
+
+    public void setArmor(String armor) {
+        this.armor = armor;
+    }
 }
