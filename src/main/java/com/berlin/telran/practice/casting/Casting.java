8544cd7e0afd88d86fec40917062105d4570bacc
nIvasiuk <nadin.ivasiuk@gmail.com>
2022-07-07 10:45:01 +0200

    add valueOf

--- src/main/java/com/berlin/telran/practice/casting/Casting.java
+++ src/main/java/com/berlin/telran/practice/casting/Casting.java
@@ -34,41 +34,56 @@ public class Casting  {
 //
 //
 
-            int a = 3;
-            double b = 4.7;
-            double c = a + b;
+//            int a = 3;
+//            double b = 4.7;
+//            double c = a + b;
+//
+//            System.out.println("double = " + c);
+//
+//            int s = 3;
+//            float f = 4.7f;
+//            float res = s + f;
+//        System.out.println("float = " + res);
+//
+//        float w = 5.6f;
+//        double g = 3.5;
+//        double h = w + g; // float h = (float) (w + g);
+//
+//        System.out.println("float -> doubel = " + h);
+//
+//        int r = 7;
+//        long l = 10;
+//        long j = r + l;
+//
+//        System.out.println("long = "  + j);
+//
+//
+//        int k = 6;
+//        float fg = 45.3f;
+//        long df = 5;
+//        double ty = 4.5;
+//        double de = k +fg+df+ty;
+//
+//        byte rt = 34;
+//        short st = 67;
+//        int sg = rt + st;
 
-            System.out.println("double = " + c);
+            char a = 'a';
+            String b = "a";
 
-            int s = 3;
-            float f = 4.7f;
-            float res = s + f;
-        System.out.println("float = " + res);
+            int c = a + 5;
+        System.out.println("char = " +c);
 
-        float w = 5.6f;
-        double g = 3.5;
-        double h = w + g; // float h = (float) (w + g);
+        char ch = '3'; // ch = 51 (accordin  ASCII table)
+        System.out.println("char value + " + ch);
 
-        System.out.println("float -> doubel = " + h);
+        int f = ch - '0'; // 51-48 (accordin  ASCII table)
+        System.out.println("int value = " + f);
 
-        int r = 7;
-        long l = 10;
-        long j = r + l;
+        // valueOf()
 
-        System.out.println("long = "  + j);
-
-
-        int k = 6;
-        float fg = 45.3f;
-        long df = 5;
-        double ty = 4.5;
-        double de = k +fg+df+ty;
-
-        byte rt = 34;
-        short st = 67;
-        int sg = rt + st;
-
-
+        int r = Integer.parseInt(String.valueOf(ch));
+        System.out.println("int value from method valueOf() = " +r);
 
 
 
