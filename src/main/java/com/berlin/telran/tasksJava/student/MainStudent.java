52800981df2fbd8e30bb08f95d3f5aa74051ae3f
nIvasiuk <nadin.ivasiuk@gmail.com>
2022-07-13 23:37:07 +0200

    create Students

--- src/main/java/com/berlin/telran/tasksJava/student/MainStudent.java
+++ src/main/java/com/berlin/telran/tasksJava/student/MainStudent.java
@@ -1,4 +1,20 @@
 package com.berlin.telran.tasksJava.student;
 
+import com.berlin.telran.tasksJava.student.Stud.Student;
+
 public class MainStudent {
+    public static void main(String[] args) {
+
+        //        In method main of the class Main create two objects of the class Student.
+//        One object with name and age and the other having name, age and address.
+
+        Student Student1 = new Student("Einstein", 20);
+        System.out.println("Student 1 :");
+        System.out.println("name " + Student1.name + ", age " + Student1.age);
+
+
+        Student Student2 = new Student("Maria Curie", 22, "France");
+        System.out.println("Student 2 :");
+        System.out.println("name " + Student2.name + ", age " + Student2.age + ", address: " + Student2.address );
+    }
 }
