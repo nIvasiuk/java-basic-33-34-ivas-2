21540521b7cec5c3c545f5bc80b1c210fdd033e7
nIvasiuk <nadin.ivasiuk@gmail.com>
2022-07-31 02:08:06 +0200

    add Pizza method

--- src/main/java/com/berlin/telran/homework/home220714/Main220714.java
+++ src/main/java/com/berlin/telran/homework/home220714/Main220714.java
@@ -36,9 +36,48 @@ public class Main220714 {
 
         System.out.println(word1.substring(word1.length() - beginWord1 , word1.length() - endWord1)+word2.substring(word2.length() - beginWord2));
 
-
-
-
-
     }
+        /*
+        Implement the program that prints to screen the results of addition, subtraction,
+        multiplication and division of two numbers.
+        Each of the arithmetic operations should be implemented as a separate method.
+         */
+
+    public static int addition(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtraction (int a, int b) {
+        return a - b;
+    }
+    public static int multiplying (int a, int b) {
+        return a * b;
+    }
+
+    public static int dividing (int a, int b){
+        return a / b;
+    }
+
+    public static double euroToUsd (double euro) {
+        double kurs = 0.98; // euro for 1 dollar
+        double euroCent = euro * 100;
+        double usdCent = Math.round( euroCent * kurs);
+        return usdCent / 100;
+    }
+
+
+    /*
+     * Write a program that calculates how many extra calories there
+     * will be if you’ll buy a pizza with a diameter of 28 cm instead of a
+     * pizza with a diameter of 24 cm. To solve this, let's assume that each
+     * square centimeter of pizza is 40 calories.
+
+     */
+    public static int calcColories (double smallDiameter, double bigDiameter) {
+        double firstSquare = Math.PI * (smallDiameter / 2) * (smallDiameter / 2);
+        double secondSquare = Math.PI * (bigDiameter / 2) * (bigDiameter / 2);
+        return (int) Math.round((bigDiameter - smallDiameter) / 40 );
+    }
+
+
 }
